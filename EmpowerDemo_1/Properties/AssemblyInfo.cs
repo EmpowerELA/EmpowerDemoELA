ae1e986a115dbadfffa4ba7ba36f931ec2a1f799
Emilie <emilie.laverge@skyline.be>
2023-03-21 15:05:41 +0100

    Add project files.

---
+++ EmpowerDemo_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerDemo_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerDemo_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("8D0488A1-A9BB-49A8-86CB-FB87CFE94D93")]
+[assembly: AssemblyVersion("1.0.0.0")]
